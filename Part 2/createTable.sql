d7c2ecb91ef996bd4f282b05feb426bd209aa57a
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2015-03-08 21:56:19 -0600

    Added sql files and data

--- Part 2/createTable.sql
+++ Part 2/createTable.sql
@@ -1,35 +1,31 @@
-drop database IF EXISTS videorental;
-
-create database IF NOT EXISTS videorental;
-
-use videorental;
+use vamshins;
 
 CREATE TABLE CUSTOMER(
-  custid Integer NOT NULL,
+  custid varchar(20) not null,
   firstname Varchar(20),
   lastname Varchar(20),
   status varchar(10),
   gender varchar(1),
   dob date,
   emailid varchar(30),
-  address varchar(50),
+  address varchar(100),
   phoneno varchar(10),
   password VARCHAR(32),
   CONSTRAINT pk_CUSTOMER PRIMARY KEY (custid)
 );
 
 CREATE TABLE GENRE(
-  genre_id Integer NOT NULL,
+  genre_id varchar(20) not null,
   name Varchar(20),
   CONSTRAINT pk_GENRE PRIMARY KEY (genre_id)
 );
 
 CREATE TABLE MOVIE(
-  movie_id Integer NOT NULL,
-  title Varchar(50) NOT NULL,
+  movie_id varchar(20) not null,
+  title Varchar(255) NOT NULL,
   director Varchar(30) NOT NULL,
   production_company Varchar(50),
-  genre_id Integer NOT NULL,
+  genre_id varchar(100) not null,
   minimal_age Integer,
   film_episode_id Integer,
   CONSTRAINT pk_FILM PRIMARY KEY (movie_id),
@@ -38,22 +34,22 @@ CREATE TABLE MOVIE(
 );
 
 CREATE TABLE CUSTOMER_RATING(
-  custid Integer NOT NULL,
-  movie_id Integer NOT NULL,
+  custid varchar(20) not null,
+  movie_id varchar(20) not null,
   rating Integer DEFAULT 3, -- Rating should be greater than 0. For this CHECK contraint has to be written. Unfortunately it is not supported in MySQL. So, this has to be handled in triggers.
   CONSTRAINT pk_CUSTOMER_RATING PRIMARY KEY (custid, movie_id),
   CONSTRAINT gives FOREIGN KEY (custid) REFERENCES CUSTOMER (custid),
-  CONSTRAINT isRated FOREIGN KEY (movie_id) REFERENCES FILM (movie_id)
+  CONSTRAINT isRated FOREIGN KEY (movie_id) REFERENCES MOVIE (movie_id)
 );
 
 CREATE TABLE MEDIUM(
-  medium_id Integer NOT NULL,
+  medium_id varchar(20) not null,
   medium_type Varchar(20),
   CONSTRAINT pk_MEDIUM PRIMARY KEY (medium_id)
 );
 
 CREATE TABLE EXEMPLAR(
-  exemplar_id Integer NOT NULL,
+  exemplar_id varchar(20) not null,
   exemplar_name varchar(30),
   address varchar(50),
   phone_no varchar(10),
@@ -61,22 +57,22 @@ CREATE TABLE EXEMPLAR(
 );
 
 create table MOVIESOFFERED(
-  offered_id Integer NOT NULL,
-  exemplar_id Integer NOT NULL,
-  movie_id Integer NOT NULL,
-  medium_id Integer NOT NULL,
+  offered_id varchar(20) not null,
+  exemplar_id varchar(20) not null,
+  movie_id varchar(20) not null,
+  medium_id varchar(20) not null,
   price_per_day DECIMAL(4,2),
   availability_status varchar(1),
   CONSTRAINT pk_moviesoffered PRIMARY KEY (offered_id),
   CONSTRAINT isOfferedBy FOREIGN KEY (exemplar_id) REFERENCES EXEMPLAR (exemplar_id),
-  CONSTRAINT isOfferedOn FOREIGN KEY (movie_id) REFERENCES FILM (movie_id),
+  CONSTRAINT isOfferedOn FOREIGN KEY (movie_id) REFERENCES MOVIE (movie_id),
   CONSTRAINT isAvailableOn FOREIGN KEY (medium_id) REFERENCES MEDIUM (medium_id)
 );
 
 CREATE TABLE BORROWING(
-  borrowing_id Integer NOT NULL,
-  offered_id Integer NOT NULL,
-  custid Integer NOT NULL,
+  borrowing_id varchar(20) not null,
+  offered_id varchar(20) not null,
+  custid varchar(20) not null,
   start_date TIMESTAMP DEFAULT NOW(), -- end_date > start_date. For this CHECK contraint has to be written. Unfortunately it is not supported in MySQL. So, this has to be handled in triggers.
   end_date Date,
   total_price DECIMAL(4,2),
